a91d5a79dde4202a4a1b075fe91b36af3793e3e2
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-12-11 17:20:44 -0600

    Como identificar al usuario autenticado

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,17 +1,26 @@
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { defineStore } from 'pinia'
 import { useFirebaseAuth } from 'vuefire'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
 
 export const useAuthStore = defineStore('auth', ()=> {
     const auth = useFirebaseAuth()
-    const authUser = ref({})
+    const authUser = ref(null)
+
     const errorMsg = ref('')
     const errorCodes = {
         'auth/user-not-found' : 'Usuario no encontrado',
         'auth/wrong-password' : 'El password es incorrecto',
         'auth/invalid-credential' : 'Credenciales invalidas'
     }
+
+    onMounted(()=> {
+        onAuthStateChanged(auth, (user)=> {
+            if(user){
+                authUser.value = user
+            }
+        })
+    })
 
     const login = ({email, password}) => {
         signInWithEmailAndPassword(auth, email, password)
