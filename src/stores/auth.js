97a1cfddc167ab951323a31635e334f2cdbf2314
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-12-11 10:30:22 -0600

    Autenticando al Usuario

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -5,7 +5,7 @@ import { signInWithEmailAndPassword } from 'firebase/auth'
 
 export const useAuthStore = defineStore('auth', ()=> {
     const auth = useFirebaseAuth()
-
+    const authUser = ref({})
     const errorMsg = ref('')
     const errorCodes = {
         'auth/user-not-found' : 'Usuario no encontrado',
@@ -16,7 +16,9 @@ export const useAuthStore = defineStore('auth', ()=> {
     const login = ({email, password}) => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential)=> {
-                console.log(userCredential)
+                const user = userCredential.user
+                authUser.value = user
+
             })
             .catch(error => {
                 errorMsg.value = errorCodes[error.code]
