b515916aca99411efe292b91818db9ee471046df
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-12-25 20:57:11 -0600

    Iterando y mostrando las propiedades

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -15,4 +15,39 @@
     >
         Nueva Propiedad
     </v-btn>
+
+
+    <v-card class="mx-auto mt-10">
+        <v-list>
+            <v-list-item
+                v-for="propiedad in propiedadesCollection"
+                :key="propiedad.id"
+            >
+                <template v-slot:prepend>
+                    <v-list-item-media start>
+                        <img width="180" :src="propiedad.imagen">
+                    </v-list-item-media>
+                </template>
+                <v-list-item-title>{{ propiedad.titulo }}</v-list-item-title>
+                <v-list-item-subtitle>{{ propiedad.precio }}</v-list-item-subtitle>
+
+                <template v-slot:append>
+                    <v-btn
+                        color="info"
+                        flat
+                        class="mr-2"
+                    >
+                        Editar
+                    </v-btn>
+                    <v-btn
+                        color="red-darken-3"
+                        flat
+                    >
+                        Eliminar
+                    </v-btn>
+                </template>
+            </v-list-item>
+        </v-list>
+    </v-card>
+
 </template>
